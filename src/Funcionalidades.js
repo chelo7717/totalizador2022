5a7bbd0482b04db98baa28b9bdb9ea1d54bf47eb
Marcelo Salinas <chelo336@gmail.com>
2022-09-19 21:35:44 -0400

    funcionalidad de precio positivo y cantidad mas html

---
+++ src/Funcionalidades.js
@@ -0,0 +1,26 @@
+const cantidad = document.querySelector("#cantidad-input");
+const precio = document.querySelector("#precio-input");
+const estado=document.querySelector("#estado-input");
+const form = document.querySelector("#totalizador-form");
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    let res= precioTotal(cantidad.value,precio.value);
+    let valorimpuesto = ImpFORstate(res,estado.value);
+    let valordescuento=DescuentoCalcular(res+valorimpuesto);
+    let porcetanjedescuento=DescuentoMostrar(res+valorimpuesto);
+    
+    document.getElementById('precio').innerHTML=res
+    document.getElementById('descuento').innerHTML=porcetanjedescuento
+    document.getElementById('impuesto').innerHTML = valorimpuesto;
+    document.getElementById('total').innerHTML = res+valorimpuesto-valordescuento;
+});
+function precioTotal(cantidad,precio)
+{
+    if((cantidad <= 0) || (precio <= 0))
+    {
+        alert("Numero negativo");
+    }
+    let res=cantidad*precio;
+    return res;
+}
