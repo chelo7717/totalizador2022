842743c347d4d838fafbd93c29088c798320bc46
Marcelo Salinas <chelo336@gmail.com>
2022-09-19 22:08:28 -0400

    arreglando errores de corchetes

--- src/Funcionalidades.js
+++ src/Funcionalidades.js
@@ -49,3 +49,54 @@ function ImpFORstate(valorcompra, valueState) {
     res = valorcompra * valorImpuestos;
     return res;
 }
+
+    function DescuentoCalcular(valorcompra){
+        let valorDescuento=0;
+        let res;
+        if(valorcompra>30000){
+            valorDescuento=0.15;
+        }else{
+            if(valorcompra>10000){
+                valorDescuento=0.1;
+            }else{
+                if(valorcompra>7000){
+                    valorDescuento=0.07;
+                }else{
+                    if(valorcompra>3000){
+                        valorDescuento=0.05;
+                    }else{
+                        if(valorcompra>1000)
+                        {
+                            valorDescuento=0.03
+                        }
+                    }
+                }
+            }
+        }
+        return res = (valorcompra*valorDescuento);
+    }
+    function DescuentoMostrar(valorcompra){
+        let valorDescuento=0;
+        let res;
+        if(valorcompra>30000){
+            valorDescuento=0.15;
+        }else{
+            if(valorcompra>10000){
+                valorDescuento=0.1;
+            }else{
+                if(valorcompra>7000){
+                    valorDescuento=0.07;
+                }else{
+                    if(valorcompra>3000){
+                        valorDescuento=0.05;
+                    }else{
+                        if(valorcompra>1000)
+                        {
+                            valorDescuento=0.03
+                        }
+                    }
+                }
+            }
+        }
+        return valorDescuento*100;
+    }
