9c454d0df26aa12f4a5ed0ca1de0c23e8bd355c4
Marcelo Salinas <chelo336@gmail.com>
2022-09-19 21:40:35 -0400

    funcionalidad importe para cada estado

--- src/Funcionalidades.js
+++ src/Funcionalidades.js
@@ -24,3 +24,28 @@ function precioTotal(cantidad,precio)
     let res=cantidad*precio;
     return res;
 }
+function ImpFORstate(valorcompra, valueState) {
+    let valorImpuestos=0;
+    let res;
+    switch (valueState) {
+        case 'UT':
+            valorImpuestos = 0.0665;
+            break;
+        case 'NV':
+            valorImpuestos = 0.08;
+            break;
+        case 'TX':
+            valorImpuestos = 0.0625;
+            break;
+        case 'AL':
+            valorImpuestos = 0.04;
+            break;
+        case 'CA':
+            valorImpuestos = 0.0825;
+            break;
+        default:
+            alert("No existe");
+    }
+    res = valorcompra * valorImpuestos;
+    return res;
+}
